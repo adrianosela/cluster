49bb58944bb85aa59437f5e99f9e86d9caa931dc
Adriano <adriano.selaviles@gmail.com>
2020-04-20 17:24:32 -0700

    changes

--- cluster.py
+++ cluster.py
@@ -6,6 +6,13 @@ import time
 # seed random number generator
 np.random.seed(int(time.time()))
 
+# colmap returns a color string to be consumed by
+# matplotlib, where n distinct, and equally separate
+# colors are chosen from the the domain of possible colors
+def colmap(i, n, mincolor, maxcolor):
+    interval = (maxcolor-mincolor)/n
+    return "#" + hex(int(mincolor+i*interval)).upper().strip("0X")
+
 class ClusterBuilder:
     def __init__(self, k, x_range, y_range):
         if len(x_range) != 2:
@@ -32,22 +39,21 @@ class ClusterBuilder:
     def stable():
         return self.stable
 
-    def step()
+    def step():
         print("not implemented")
         # TODO: implement k-means algorithm step 
 
     def build(self):
         while not stable(self):
-            step(self)
+            self.step()
     
     def plot(self):
         df = pd.DataFrame({'x': self.x_vals, 'y': self.y_vals})
         fig = plt.figure(figsize=(5, 5))
 
-        colmap = {1: 'r', 2: 'g', 3: 'b'}
         plt.scatter(df['x'], df['y'], color='k')
         for k in self.centroids:
-            plt.scatter(*self.centroids[k], color=colmap[k])
+            plt.scatter(*self.centroids[k], color=colmap(k, len(self.centroids), 0x000000, 0xffffff))
 
         plt.xlim(self.x_range[0], self.x_range[1])
         plt.ylim(self.y_range[0], self.y_range[1])
@@ -55,5 +61,4 @@ class ClusterBuilder:
 
 x = ClusterBuilder(3, (0, 80), (0, 80))
 x.feed([12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72], [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24])
-x.build()
 x.plot()
